e8d3ee356f52036d32c5c374a07d590152a75afa
hauntmuskie <rioclasher4@gmail.com>
2025-07-11 23:55:14 +0700

    feat: Migrate database connection from MySQL to TiDB serverless and update configuration

--- database/index.ts
+++ database/index.ts
@@ -1,19 +1,10 @@
-import { drizzle } from "drizzle-orm/mysql2";
-import mysql from "mysql2/promise";
+import { drizzle } from "drizzle-orm/tidb-serverless";
 
 import * as schema from "./schema";
 
-const poolConnection = mysql.createPool({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "",
-  database: process.env.DB_NAME || "db_pemilihan_cat_dinding",
-  port: Number(process.env.DB_PORT) || 3306,
-});
-
-export const db = drizzle(poolConnection, {
+export const db = drizzle({
+  connection: { url: process.env.TIDB_URL! },
   schema,
-  mode: "default",
 });
 
 export default db;
