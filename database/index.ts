0877c84ee171ef71eb1f6c8c7f396c9361a9b394
hauntmuskie <rioclasher4@gmail.com>
2025-07-18 09:33:45 +0700

    feat: streamline database connection setup; remove unused MySQL connection logic and simplify export

--- database/index.ts
+++ database/index.ts
@@ -1,46 +1,32 @@
-// import { drizzle } from "drizzle-orm/tidb-serverless";
-
-// import * as schema from "./schema";
-
-// export const db = drizzle({
-//   connection: { url: process.env.TIDB_URL! },
-//   schema,
-// });
-
-// export default db;
+import { connect } from "@tidbcloud/serverless";
+import { drizzle } from "drizzle-orm/tidb-serverless";
 
 import * as schema from "./schema";
 
-let db;
-if (process.env.NODE_ENV === "production") {
-  // TiDB in production
-  // Use drizzle-orm/tidb-serverless
-  // https://orm.drizzle.team/docs/tidb-serverless
-  // TiDB_URL example: mysql://user:password@host:port/dbname
-  // Make sure to install drizzle-orm@latest and @tidbcloud/serverless
-  // and set TIDB_URL in your production environment
-  // @ts-ignore
-  const { drizzle } = require("drizzle-orm/tidb-serverless");
-  db = drizzle({
-    connection: { url: process.env.TIDB_URL },
-    schema,
-  });
-} else {
-  // MySQL in development
-  const { drizzle } = require("drizzle-orm/mysql2");
-  const mysql = require("mysql2/promise");
-  const poolConnection = mysql.createPool({
-    host: process.env.DB_HOST || "localhost",
-    user: process.env.DB_USER || "root",
-    password: process.env.DB_PASSWORD || "",
-    database: process.env.DB_NAME || "db_pemilihan_cat_dinding",
-    port: Number(process.env.DB_PORT) || 3306,
-  });
-  db = drizzle(poolConnection, {
-    schema,
-    mode: "default",
-  });
-}
+const client = connect({ url: process.env.TIDB_URL });
+const db = drizzle({
+  client,
+  schema,
+});
 
 export { db };
-export default db;
+
+// import { drizzle } from "drizzle-orm/mysql2";
+// import mysql from "mysql2/promise";
+
+// import * as schema from "./schema";
+
+// const poolConnection = mysql.createPool({
+//   host: process.env.DB_HOST || "localhost",
+//   user: process.env.DB_USER || "root",
+//   password: process.env.DB_PASSWORD || "",
+//   database: process.env.DB_NAME || "db_pemilihan_cat_dinding",
+//   port: Number(process.env.DB_PORT) || 3306,
+// });
+
+// export const db = drizzle(poolConnection, {
+//   schema,
+//   mode: "default",
+// });
+
+// export { db };
