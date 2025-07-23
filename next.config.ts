fe322d9e56e6f4aadd6ec4cfd076ca4ab444d8e3
hauntmuskie <rioclasher4@gmail.com>
2025-07-23 20:52:20 +0700

    feat: add experimental server actions configuration with allowed origins

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    serverActions: {
+      allowedOrigins: ["https://wp-amel.vercel.app/"],
+    },
+  },
 };
 
 export default nextConfig;
